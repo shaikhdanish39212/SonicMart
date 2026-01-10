f9ba393d156e86ea06a8b3087e5c3ed36d996bf2
shaikhdanish3921 <148886281+shaikhdanish39212@users.noreply.github.com>
2026-01-10 16:02:33 +0530

    Fix: Sync package-lock.json and use ESM import for daisyui

--- myproject/tailwind.config.js
+++ myproject/tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -25,5 +27,5 @@ export default {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 }
